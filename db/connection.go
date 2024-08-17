3c25257c03d6944d727e61d1b0b88e87a475cbf5
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-08-17 17:03:47 -0400

    feat: :sparkles: create user model

---
+++ db/connection.go
@@ -0,0 +1,21 @@
+package db
+
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DSN = "host=localhost user=postgres password=alejandrobravo09 dbname=gorm port=5432 sslmode=disable"
+var DB *gorm.DB
+
+func DBConnection() {
+	var error error
+	DB, error = gorm.Open(postgres.Open(DSN), &gorm.Config{})
+	if error != nil {
+		log.Fatal(error)
+	} else {
+		log.Println("DB Connected")
+	}
+}
--- main.go
+++ main.go
@@ -4,10 +4,18 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/salvadorbravo09/go-gorm-restapi/db"
+	"github.com/salvadorbravo09/go-gorm-restapi/models"
 	"github.com/salvadorbravo09/go-gorm-restapi/routes"
 )
 
 func main() {
+
+	db.DBConnection()
+
+	db.DB.AutoMigrate(models.Task{})
+	db.DB.AutoMigrate(models.User{})
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", routes.HomeHandler)
 
